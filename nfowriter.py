bdadb162a0b006663077927afbe167c08dbf8920
Vd <vadyur@csodessa.com>
2015-12-14 14:07:59 +0200

    Fix toprettyxml

--- nfowriter.py
+++ nfowriter.py
@@ -1,12 +1,43 @@
 import xml.etree.ElementTree as ET
 import xml.dom.minidom as minidom
+from xml.dom.minidom import *
 from base import *
 from tvshowapi import *
 from movieapi import *
 
+def fixed_writexml(self, writer, indent="", addindent="", newl=""):
+	# indent = current indentation
+	# addindent = indentation to add to higher levels
+	# newl = newline string
+	writer.write(indent+"<" + self.tagName)
+
+	attrs = self._get_attributes()
+	a_names = attrs.keys()
+	a_names.sort()
+
+	for a_name in a_names:
+		writer.write(" %s=\"" % a_name)
+		xml.dom.minidom._write_data(writer, attrs[a_name].value)
+		writer.write("\"")
+	if self.childNodes:
+		if len(self.childNodes) == 1 \
+		  and self.childNodes[0].nodeType == xml.dom.minidom.Node.TEXT_NODE:
+			writer.write(">")
+			self.childNodes[0].writexml(writer, "", "", "")
+			writer.write("</%s>%s" % (self.tagName, newl))
+			return
+		writer.write(">%s"%(newl))
+		for node in self.childNodes:
+			node.writexml(writer,indent+addindent,addindent,newl)
+		writer.write("%s</%s>%s" % (indent,self.tagName,newl))
+	else:
+		writer.write("/>%s"%(newl))
+# replace minidom's function with ours
+xml.dom.minidom.Element.writexml = fixed_writexml
+
 def prettify(xml_text):
     reparsed = minidom.parseString(xml_text)
-    return reparsed.toprettyxml(indent="\t", encoding="utf-8")
+    return reparsed.toprettyxml(indent=" "*2, encoding="utf-8")
 
 def write_tree(fn, root):
 	try:
