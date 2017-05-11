bfcd214bd277a87e0c1d2cbcd81ee2d147e4730a
Vd <vadyur@csodessa.com>
2017-05-11 15:02:19 +0300

    Fix context search

--- context_search.py
+++ context_search.py
@@ -2,7 +2,12 @@ import urllib2, xbmc
 
 def main():
 	Label = xbmc.getInfoLabel("ListItem.Label")
-	xbmc.executebuiltin("XBMC.ActivateWindow(Video, plugin://script.media.aggregator/?action=search_context&s=%s, return)" % (urllib2.quote(Label)))
+	#xbmc.executebuiltin("XBMC.ActivateWindow(Video, plugin://script.media.aggregator/?action=search_context&s=%s, return)" % (urllib2.quote(Label)))
+	command = 'plugin://script.media.aggregator/?action=search&keyword=' + urllib2.quote(Label)
+	xbmc.executebuiltin(b'Container.Update(\"%s\")' % command)
 
 if __name__ == '__main__':
+	#import brkpnt
+	#brkpnt._bp()
+
 	main()
