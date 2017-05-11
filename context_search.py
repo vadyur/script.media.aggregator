d645e1d8f14264b617718f8c21d06888a18bf4e3
Vd <vadyur@csodessa.com>
2017-05-11 17:59:13 +0300

    impove addons compatible

--- context_search.py
+++ context_search.py
@@ -1,7 +1,9 @@
-import urllib2, xbmc
+import urllib2, xbmc, re
 
 def main():
 	Label = xbmc.getInfoLabel("ListItem.Label")
+	Label = re.sub('\[.+\]', '', Label).strip()
+
 	#xbmc.executebuiltin("XBMC.ActivateWindow(Video, plugin://script.media.aggregator/?action=search_context&s=%s, return)" % (urllib2.quote(Label)))
 	command = 'plugin://script.media.aggregator/?action=search&keyword=' + urllib2.quote(Label)
 	xbmc.executebuiltin(b'Container.Update(\"%s\")' % command)
